fc83a7bef471f564c56532e669e7cc1584ee65ee
Stephen Ray <SR2610Email@Gmail.com>
2015-02-05 12:18:06 +0000

    moving Variables to different files

--- src/main/java/com/sr2610/alignment/Alignment.java
+++ src/main/java/com/sr2610/alignment/Alignment.java
@@ -1,9 +1,11 @@
 package com.sr2610.alignment;
 
+import com.sr2610.alignment.common.lib.LibNames;
+
 import cpw.mods.fml.common.Mod;
 
 
-@Mod(modid = "Alignment", name = "Alignment", version = "1.0")
+@Mod(modid = LibNames.modID, name = LibNames.modName, version = LibNames.modVersion)
 public class Alignment {
 
 }
---
+++ src/main/java/com/sr2610/alignment/common/lib/LibNames.java
@@ -0,0 +1,8 @@
+package com.sr2610.alignment.common.lib;
+
+public final class LibNames {
+public static final  String modName = "Alignment";
+public static final String modID = "alignment";
+public static final String modVersion = "1.0";
+
+}
